2b2f0c9f0a8d7de8398f3e8d767c77357e8ff535
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-05-16 13:51:44 +0700

    Updates

--- lib/Log/ger/Plugin/Multisets.pm
+++ lib/Log/ger/Plugin/Multisets.pm
@@ -1,14 +1,14 @@
 package Log::ger::Plugin::Multisets;
 
-# AUTHORITY
-# DATE
-# DIST
-# VERSION
-
 use strict;
 use warnings;
 
 use Log::ger ();
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
 
 sub meta { +{
     v => 2,
